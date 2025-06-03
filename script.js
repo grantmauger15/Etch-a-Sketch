082e7495f5872ecdd73efea73a19808571194ddd
grantmauger15 <grantmauger15@gmail.com>
2025-06-02 18:44:32 -0700

    Add resize button

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 const container = document.querySelector("#container");
 
-for(let i = 0; i < 16 * 16; i++) {
-    let cell = document.createElement('div');
-    cell.setAttribute("class", "cell");
-    container.appendChild(cell);
+function generateGrid(size) {
+    for(let i = 0; i < size * size; i++) {
+        let cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.style.flex = `0 0 ${100 / size}%`;
+        container.appendChild(cell);
+    }
+
+    document.querySelectorAll('.cell').forEach(cell => {
+        cell.addEventListener('mouseover', (e) => {
+            if(!e.target.classList.contains("fill")) {
+                e.target.classList.add("fill");
+            }
+        });
+    });
 }
 
-document.querySelectorAll('.cell').forEach(cell => {
-    cell.addEventListener('mouseover', (e) => {
-        if(!e.target.classList.contains("fill")) {
-            e.target.classList.add("fill");
+document.querySelector('#sizeButton').addEventListener('click', () => {
+        let newSize = +prompt("Enter a new size value for the grid.");
+        if(!newSize || !Number.isInteger(newSize) || newSize < 1) return;
+        if(newSize > 100) {
+            newSize = 100;
         }
-    });
+        container.replaceChildren();
+        generateGrid(newSize);
 });
+
+generateGrid(16);
